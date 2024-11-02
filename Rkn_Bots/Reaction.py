635fd688d4f43d12590a4d771bf02c996ef828b7
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2024-11-02 09:00:59 +0530

    Update Reaction.py

--- Rkn_Bots/Reaction.py
+++ Rkn_Bots/Reaction.py
@@ -122,3 +122,28 @@ async def group_start_cmd(bot, message):
 @Client.on_message(filters.all)
 async def send_reaction(bot, message):
     await react_msg(bot, message)
+
+#---------------------------Fun.py----------------(((((((
+@Client.on_message(filters.command("dice"))
+async def roll_dice(bot, message):
+    await bot.send_dice(message.chat.id, "🎲")
+
+@Client.on_message(filters.command("arrow"))                                      
+async def roll_arrow(bot, message):
+    await bot.send_dice(message.chat.id, "🎯")
+
+@Client.on_message(filters.command("goal"))
+async def roll_goal(bot, message):
+    await bot.send_dice(message.chat.id, "⚽️")
+
+@Client.on_message(filters.command("luck"))
+async def roll_luck(bot, message):
+    await bot.send_dice(message.chat.id, "🎰")
+
+@Client.on_message(filters.command("throw"))
+async def roll_throw(bot, message):
+    await bot.send_dice(message.chat.id, "🏀")
+
+@Client.on_message(filters.command(["bowling", "tenpins"]))
+async def roll_bowling(bot, message):
+    await bot.send_dice(message.chat.id, "🎳")
